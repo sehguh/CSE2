1b0a96b4832e80b9943e4402ee02f991445b628e
Ryan Hughes <rmh318@lehigh.edu>
2015-01-23 19:54:57 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,24 @@
+//Ryan Hughes
+//Hello World java program
+//
+//  first compile the program
+//      javac HelloWorld.java 
+//  run the program
+//  java HelloWorld
+
+//  Define a class
+    public class HelloWorld {
+        
+        public static void main(String[] args) {
+            System.out.println("########################");
+            System.out.println("########################");
+            System.out.println("#                      #");
+            System.out.println("#  Hello World 2014!   #");
+            System.out.println("#                      #");
+            System.out.println("########################");
+            System.out.println("########################");
+            System.out.println("Test");
+            
+            
+        }
+    }
