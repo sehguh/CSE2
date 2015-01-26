0eb80ae25021bd6169b1524a25e3caadccb013f1
Ryan Hughes <rmh318@lehigh.edu>
2015-01-26 00:22:41 +0000

    updated welcome

---
+++ hw01/welcome.java
@@ -0,0 +1,20 @@
+// Ryan Hughes
+// January 25 2015
+// Cse 02
+
+    public class welcome {
+        
+        public static void main (String [] args) {
+            
+            System.out.println(" -----------");
+            System.out.println(" | WELCOME |");
+            System.out.println(" -----------");
+            System.out.println("  ^  ^  ^  ^  ^  ^");
+            System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+            System.out.println("<-R--M--H--3--1--8->");
+            System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+            System.out.println("  v  v  v  v  v  v");
+            System.out.println("This July I went to Florida");
+            System.out.println("to visit my grandparents");
+        }
+    }
