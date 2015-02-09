23e86a2a7bf5f4b1f44d2fb9a160f0c8cd6d6783
Ryan Hughes <rmh318@lehigh.edu>
2015-02-09 22:45:43 +0000

    updated FourDigits

--- hw03/FourDigits.java
+++ hw03/FourDigits.java
@@ -2,33 +2,33 @@
 //02/09/15
 //cse002
 //Program that prints out the first four decimal places
-import java.util.Scanner;
-    public class FourDigits {
-        public static void main(String [] args){
+import java.util.Scanner; //imports the scanner
+    public class FourDigits {//declares the class
+        public static void main(String [] args){//declares the main method to be used
         
-        Scanner myScanner;
+        Scanner myScanner;//declares the scanner
         myScanner = new Scanner(System.in);
-        System.out.println("Enter a double and I display the four digits to the right of the decimal point:");
-        double number = myScanner.nextDouble();
+        System.out.println("Enter a double and I display the four digits to the right of the decimal point:");//asks the user to input the data
+        double number = myScanner.nextDouble();//decalres a double fro the inputted data
         
-        int integer = (int)number;
+        int integer = (int)number;//converts to an integer
         
-        double decimals = number - integer;
+        double decimals = number - integer;//gets the decimals by themselves
         decimals *= 10000;
-        decimals = (int)decimals;
         
-        int first = (0) + (decimals /= 1000);
+        
+        int first = 0 + ((int)decimals /= 1000);//locates the first digit and saves it
         decimals = decimals - (first * 1000);
         
-        int second = 0 + (decimals /= 100);
+        int second = 0 + ((int)decimals /= 100);//locates the second digit and saves it
         decimals = decimals - (second * 100);
         
-        int third = 0 + (decimals /= 10);
+        int third = 0 + ((int)decimals /= 10);//locates the third digit and saves it
         decimals = decimals - (third * 10);
         
-        int fourth = 0 + decimals;
+        int fourth = 0 + (int)decimals;//locates the fourth digit and saves it
         
-        System.out.println("The four digits are: " + first + ", " + second + ", " + third + ", " + fourth);
+        System.out.println("The four digits are: " + first + ", " + second + ", " + third + ", " + fourth);//outputs the four digits
       
         }
         
