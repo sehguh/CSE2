415dcc5c8ccc9d46041b55a77438ee3dbf38c0fe
Ryan Hughes <rmh318@lehigh.edu>
2015-03-25 02:28:57 +0000

    updated HW8

--- hw08/HW8.java
+++ hw08/HW8.java
@@ -2,10 +2,10 @@
 //03//22/15
 //CSE002
 //Program that allows the user to play a game
-import java.util.Scanner;
-import java.util.Random;
-    public class HW8 {
-        public static void main(String[] args) {
+import java.util.Scanner;//imports the scanner
+import java.util.Random;//imports the random function
+    public class HW8 {//declares the class
+        public static void main(String[] args) {//declares this as the main method
             char option;
             Scanner scan = new Scanner(System.in);
             
@@ -33,70 +33,70 @@ import java.util.Random;
             System.out.println("Hero! Have a good day!");
         }
         
-        public static String getInput(Scanner scan, String string) {
-            String input = scan.next();
+        public static String getInput(Scanner scan, String string) {//this method determines whether the user wants to continue
+            String input = scan.next();//declares and initializes a scanner
             
-            if(!input.equals("C") && !input.equals("c")) {
+            if(!input.equals("C") && !input.equals("c")) {//if the user has given incorrect input the the program exits
                 System.out.println("Yeah Right Loser!");
-                System.exit(0);
+                System.exit(0);//the program exits
             }
-            return input;
+            return input;//returns to the main method
         }    
         
-        public static String getInput(Scanner scan, String string, int trial) {
+        public static String getInput(Scanner scan, String string, int trial) {//this method simulates the battle with the giant
             
-           int g = 0;
+           int g = 0;//declares and initializes integers to be used later
            int t = 0;
            String input = "r";
-           Random rand = new Random();
+           Random rand = new Random();//declares a random variable
            
            
-           while(t < 10){
-                if(g != 0) {
-                    System.out.println("Enter 'A' or 'a' to Attack, 'E' or 'e' to Escape, ANYTHING else is to YIELD");
+           while(t < 10){//while loop to keep track of the number of crits on the giant
+                if(g != 0) {//this allows the user to have the question repeated to them and doesnt say it twice the forst time the method is called
+                    System.out.println("Enter 'A' or 'a' to Attack, 'E' or 'e' to Escape, ANYTHING else is to YIELD");//asks the user for input
                 }
-                    input = scan.next();
-                if(!input.equals("A") && !input.equals("a") && !input.equals("E") && !input.equals("e")) {
-                    System.out.println("Executed by the GIANT! GAME OVER!");
-                    System.exit(0);
+                    input = scan.next();//the next input will be equal to the input
+                if(!input.equals("A") && !input.equals("a") && !input.equals("E") && !input.equals("e")) {//checks to make sure Aa Ee was input
+                    System.out.println("Executed by the GIANT! GAME OVER!");//if not the game ends
+                    System.exit(0);//the program quits
                     }
                     
-                    if(input.equals("a") || input.equals("A")) {
-                        int x = rand.nextInt(2);
-                            if(x == 0){
+                    if(input.equals("a") || input.equals("A")) {//this is the code for the attack move
+                        int x = rand.nextInt(2);//generates a random number between 0 and 1
+                            if(x == 0){//if x is 0 this runs
                                 System.out.println("Critical hit!");
-                                t++;
-                                g++;
+                                t++;//increments t
+                                g++;//increments g
                             } else {
-                                System.out.println("How can you miss!?");
-                                g++;
-                                continue;
+                                System.out.println("How can you miss!?");//if  the other number is print then a miss is had
+                                g++;//increments g
+                                continue;//continues to the top of the loop
                                 
                             }
                         } else {
-                            int y = rand.nextInt(11);
+                            int y = rand.nextInt(11);//generates a random number between 1 and 10
                             if(y == 10){
-                                System.out.println("You have escaped successfully!");
-                                System.exit(0);
+                                System.out.println("You have escaped successfully!");//only if it is 10 can you escae successfully
+                                System.exit(0);//the program ends
                             } else {
                                 System.out.println("You failed to escape");
-                                continue;
+                                continue;//otherwise the loop starts again at the top
                             }
                         }   
                 
            }
            System.out.println("YOU HAVE SLAIN THE GIANT!");
            
-           return input;
+           return input;//returns to the main method
         }
         
-        public static String getInput(Scanner scan) {
-            String input = "r";
+        public static String getInput(Scanner scan) {//this method determines the final prize
+            String input = "r";//declares a string
             String z = scan.next();
-            switch(z) {
+            switch(z) {//starts a switch statement
                 case "1": 
-                    System.out.println("You have been given a never ending supply of AA batteries");
-                    break;
+                    System.out.println("You have been given a never ending supply of AA batteries");//if z is 1 this is printed
+                    break;//ends the switch statement
                 case "2": 
                     System.out.println("You have received a flashlight that only works in the presence of sunlight");
                     break;
@@ -107,7 +107,7 @@ import java.util.Random;
                     System.out.println("You chose a wrong number, you get nothing, now go and start again :p");
                     break;
             }
-            return input;
+            return input;//returns to the main method
         }
         
          
